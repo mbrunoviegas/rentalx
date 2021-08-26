187c187d4511e4a5916f43d81af77415f60b6873
Marcelo Viegas <mbrunoviegas@gmail.com>
2021-08-26 16:54:08 -0300

    Adding sentry

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -6,6 +6,8 @@ import path from 'path';
 import 'reflect-metadata';
 import swaggerUi from 'swagger-ui-express';
 import YAML from 'yamljs';
+import * as Sentry from '@sentry/node';
+import * as Tracing from '@sentry/tracing';
 import createConnection from '@shared/infra/database';
 import routes from '@shared/infra/http/routes';
 import { ExceptionValidation } from '../../core/errors/ExceptionValidation';
@@ -13,6 +15,19 @@ import rateLimiter from './middlewares/RateLimiter';
 
 createConnection();
 const app = express();
+
+Sentry.init({
+  dsn: 'https://05a52653bfa2432ebf65aaafe5380f39@o976391.ingest.sentry.io/5932741',
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+    new Tracing.Integrations.Express({ app }),
+  ],
+
+  tracesSampleRate: 1.0,
+});
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
 
 app.use(rateLimiter);
 
@@ -23,6 +38,8 @@ app.use('/api', swaggerUi.serve, swaggerUi.setup(YAML.load(swaggerDocumentPath))
 
 app.use(cors());
 app.use(routes);
+
+app.use(Sentry.Handlers.errorHandler());
 
 const exceptionValidation = new ExceptionValidation();
 app.use(exceptionValidation.catchError);
